7e7804fa7a502585bc23412ab9f6c6e65cb66f60
Dueling-W <127268061+Dueling-W@users.noreply.github.com>
2023-12-11 22:37:09 -0500

    Create utils.js
    utils.js use PogObject library to load json files (data.json used utils.js as a dependency)

---
+++ utils.js
@@ -0,0 +1,14 @@
+import PogObject from "../PogData";
+
+export let data = new PogObject("InkUtilities", {
+    "SCC": {
+        "squidAmount": 0,
+        "inkAmount": 0,
+        "nightSquidCaught": 0,
+        "x": 150,
+        "y": 150,
+        "looting": 5,
+        "squidInkNum": 8.75,
+        "nightSquidInkNum": 40
+    }
+}, "data.json")
