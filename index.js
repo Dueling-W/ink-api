5340f2a44ad0a4004cc20b936d50ae726fe8b0ef
Dueling-W <127268061+Dueling-W@users.noreply.github.com>
2023-12-11 20:40:08 -0500

    Update index.js
    updated index.js to only have initial help commands, ink commands have been moved to inkTracking.js

--- index.js
+++ index.js
@@ -1,77 +1,75 @@
-//Sea Creature Dictionary (TEMPORARY VALUES NEED TO MAKE A JSON FILE)
-let SCC = {
-    squidAmount: 10,
-    inkAmount: 0,
-    nightSquidCaught: 0,
-    ttlWaterCreatures: 0,
-    agarimooCaught: 0,
-    x: 150,
-    y: 150
-};
-
-//Create a display, background is set per line, render display at the x and y coordinates
-var display = new Display();
-display.setBackground("per line");
-display.setRenderLoc(SCC.x,SCC.y);
-
-//Trigger var for starting/stopping ink tracking
-var value = -1;
-
-//Register commands to start/stop ink tracking
-register("command", startTracking).setName("ink_go");
-register("command", stopTracking).setName("ink_stop");
+import "./inkTracking.js"
 
 
-register("command", (x,y) => {
-    x = parseInt(x);
-    y = parseInt(y);
+//========STARTING AND MISC. COMMANDS========
 
-    updateDisplay(x, y);
-    ChatLib.chat("&aDisplay moved to: " + x + ", " + y);
+//Ink Utilities help starting command
+register("command", () => {
+    ChatLib.chat("&f&l----------Ink Utilities----------&r");
+    ChatLib.chat("&6Thank you for Downloading &r&f&nInkUtils&r&6!&r");
+    ChatLib.chat("&6You can open view all commands by using /inkCommands | /inkHelp&r");
+    ChatLib.chat("&6Please use a level 100 epic or legendary squid pet for accurate rates!");
+    ChatLib.chat("&6Put any questions on the GitHub or message at #lowbudget on discord!");
+    ChatLib.chat("&6That's all, happy inking!");
 
-}).setName("moveDisplay");
-
-
-//Accounces ink tracking will start
-function startTracking() {
-    ChatLib.chat("Now Tracking Squids and Ink!!");
-    value = 0;
-}
-
-//Accounces ink tracking will stop
-function stopTracking() {
-    ChatLib.chat("Squid and Ink Tracking Stopped.");
-    value = -1;
-}
-
-//Increments squid tracker/ink tracker
-register("chat", () => {
-    if(!value==0) return;
-
-    SCC.squidAmount += 1;
-    //Based on Looting 5 + Squid Pet (will be changed to base on sacks soon TM)
-    SCC.inkAmount += 8.75;
-
-}).setCriteria("A Squid appeared.");
+}).setName("inkUtils").setAliases("ink");
 
 
-//Render the display overlay (currently only squid amount and ink amount)
-register ("renderoverlay", () => {
-    if(!value==0) return;
-    var inkValue = SCC.inkAmount;
-    inkString = inkValue.toString();
+//All registered commands
+register("command", () => {
+    ChatLib.chat("&1&l------Ink Utils Command List------");
+    ChatLib.chat("&b--Use /inkGo to begin tracking squid, night squids, and ink gain (shows display)!--");
+    ChatLib.chat("&b--Use /inkStop to stop tracking squids, night squids, and ink gain (hides display)!--");
+    ChatLib.chat("&b--Use /moveDisplay (x) (y) to move the display!--");
+    ChatLib.chat("&b--Use /screenXY to get the dimensions of your screen (helpful for /moveDisplay)!--");
+    ChatLib.chat("&b--Use /looting (4 or 5) to set your looting enchant!--");
+    ChatLib.chat("&b--Use /track (stop or start) to stop or start timer, will also pause ink/hr calculations!--")
 
-    var squidAmount = SCC.squidAmount;
-    squidString = squidAmount.toString();
-
-    display.setLine(0, "&1" + "Ink Info Display!");
-    display.setLine(1, "&6" + inkString);
-    display.setLine(2, "&b" + squidString);
-})
+}).setName("inkCommands").setAliases("inkHelp");
 
 
-function updateDisplay(x, y) {
-    displayX = x;
-    displayY = y;
-    display.setRenderLoc(displayX, displayY);
-}
+
+//Register command to get width and height of screen
+register("command", () => {
+    var width = Renderer.screen.getWidth();
+    var height = Renderer.screen.getHeight();
+
+    ChatLib.chat("&bThe width of your screen is " + width + ", the height of your screen is " + height);
+
+}).setName("screenXY");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
