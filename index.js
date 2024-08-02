a8a1bd47a8aaf7156d50ba41c107c2847d679b23
Dueling-W <127268061+Dueling-W@users.noreply.github.com>
2024-08-01 20:28:41 -0400

    Update index.js
    fixed wording on commands, new commands, auto-party feature added

--- index.js
+++ index.js
@@ -5,58 +5,78 @@ import "./inkTracking.js"
 import "./sccTracker.js"
 import "./apiCalls.js"
 import "./bestiary.js"
+import "./spookyTracking.js"
 import {data} from "./utils.js"
 import settings from "./settings"
 
 
 
-//========STARTING AND MISC. COMMANDS========
+//========FIRST START MESSAGES========
 
 register("command", () => settings.openGUI()).setName("inkUtils").setAliases("Ink");
 
 if (settings.firstRun) {
     settings.firstRun = false;
     ChatLib.chat("&b&l----------InkUtilities----------")
-    ChatLib.chat("&9Thank you for Downloading &f&nInkUtilities!");
-    ChatLib.chat("&9You can open the settings using /ink | /inkUtils");
-    ChatLib.chat("&9View all commands with /inkCommands | /inkHelp");
-    ChatLib.chat("&9Please use a level 100 epic or legendary squid for accurate rates!");
-    ChatLib.chat("&9Set your looting enchant using /looting (4) or (5)!");
+    ChatLib.chat("&aThank you for Downloading &f&nInkUtilities!");
+    ChatLib.chat("&aYou can open the settings using /ink | /inkUtils");
+    ChatLib.chat("&aView all commands with /inkCommands | /inkHelp");
+    ChatLib.chat("&aPlease use a level 100 epic or legendary squid for accurate rates!");
+    ChatLib.chat("&aSet your looting enchant using /looting (4) or (5)!");
 }
 
 
+//========REGISTERED COMMAND LIST========
 
 register("command", () => {
     ChatLib.chat("&b&l----------InkUtilities Commands----------");
-    ChatLib.chat("&9Use /moveinkdisplay to move the ink display (display needs to be open)");
-    ChatLib.chat("&9Use /movescdisplay to move the sea creature display");
-    ChatLib.chat("&9Use /movemoneydisplay to move the money display");
-    ChatLib.chat("&9Use /movebestdisplay to move the bestiary display");
-    ChatLib.chat("&9Use /inkGui to open a GUI of ink related stats");
-    ChatLib.chat("&9Use /scGui to open a GUI of sea creature related stats");
-    ChatLib.chat("&9Use /setInk (value) to set your overall ink collection");
-    ChatLib.chat("&9Use /setSquid (value) to set your overall squid kills");
-    ChatLib.chat("&9Use /setNightSquid (value) to set your overall night squid kills");
-    ChatLib.chat("&9Use /setSC (value) to set your total sea creature kills");
-    ChatLib.chat("&9Use /setEmp (value) to set your total sea emperor kills");
-    ChatLib.chat("&9Use /setHydra (value) to set your total water hydra kills");
-    ChatLib.chat("&9Use /setMoo (value) to set your total agarimoo kills");
-    ChatLib.chat("&9Use /setCarrot (value) to set your total carrot king kills");
-    ChatLib.chat("&9Use /setRabbit (value) to set your total oasis rabbit kills");
-    ChatLib.chat("&9Use /setSheep (value) to set your total oasis sheep kills");
-    ChatLib.chat("&9Use /setWorm (value) to set your total water worm kills");
-    ChatLib.chat("&9Use /setPoisonWorm (value) to set your total poisoned water worm kills");
-    ChatLib.chat("&9Use /setZombie (value) to set your total zombie miner kills");
-    ChatLib.chat("&9Note: all /set commands require /ct load to update")
+    ChatLib.chat("&aUse /moveinkdisplay to move the ink display (display needs to be open)");
+    ChatLib.chat("&aUse /movescdisplay to move the sea creature display");
+    ChatLib.chat("&aUse /movemoneydisplay to move the money display");
+    ChatLib.chat("&aUse /movebestdisplay to move the bestiary display");
+    ChatLib.chat("&aUse /movespookydisplay to move the spooky display");
+    ChatLib.chat("&aUse /movegoaldisplay to move the ink goal display");
+    ChatLib.chat("&aUse /inkGui to open a temporary GUI of ink related stats");
+    ChatLib.chat("&aUse /scGui to open a temporary GUI of sea creature related stats");
+    ChatLib.chat("&aUse /resetTimer to reset session timer");
+    ChatLib.chat("&aUse /set(condition) (value) to set your lifestime fishing stats");
+    ChatLib.chat("&aValid conditions are: sc, emp, hydra, moo, carrot, ink, squid, nightSquid, rabbit, sheep, worm, poisonWorm, miner, scare, nightmare, werewolf, phantom, grim");
 
 }).setName("inkCommands").setAliases("inkHelp");
 
 
 
+
+
+
+
+
+
+
+//========AUTO-PARTY FEATURE========
+register("chat", (msg) => {
+
+    if(!settings.autoParty) return;
+
+    msg = msg.toLowerCase();
+    pwrd = settings.partyMessage;
+
+    const regex = /\] ([^:]+):/;
+    const match = msg.match(regex);
+
+    if(msg.includes(pwrd)) {
+        const ign = match[1].trim();
+        ChatLib.say(`/party ${ign}`)
+    }
+
+
+
+}).setCriteria("From ${message}");
+
+
+
+
 //========SET LOOTING========
-//
-//
-//
 register("command", (x) => {
 
     x = parseInt(x);
@@ -71,7 +91,7 @@ register("command", (x) => {
     } else if(x == 5) {
         data.INK.looting = 5;
         data.INK.squidInkNum =8.75;
-        data.INK.nightSquidInkNum = 40;
+        data.INK.nightSquidInkNum = 40.0572;
         data.SCC.mooDrop = 5.25;
         data.save();
         ChatLib.chat("&bYour looting enchant has been set to " + x + "!");
